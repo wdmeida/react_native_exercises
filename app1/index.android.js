5f67fc7bf199ade2ea4ae501c598c996c7057945
Wagner Almeida <wdmeida@gmail.com>
2017-04-20 22:33:46 -0300

    Atualização do projeto.

--- app1/index.android.js
+++ app1/index.android.js
@@ -1,11 +1,26 @@
 var React = require('react');
 var Text = require('react-native').Text;
+var View = require('react-native').View;
+var Button = require('react-native').Button;
 var AppRegistry = require('react-native').AppRegistry;
 
-const App = function() {
-  return (
-    <Text>Meu primeiro APP</Text>
-  );
-};
+const gerarNumeroAleatorio = () => {
+  var numero_aleatorio = Math.random();
+  
+  numero_aleatorio = Math.floor(numero_aleatorio * 10);
+  alert(numero_aleatorio);
+}
 
-AppRegistry.registerComponent('app1', function() { return App });
+const App = () => {
+  return (
+    <View>
+      <Text>Gerador de números randômicos</Text>
+      <Button 
+        title = "Gerar um número randômico"
+        onPress={gerarNumeroAleatorio}
+      />
+    </View>
+  )
+}
+
+AppRegistry.registerComponent('app1', () =>  App );
