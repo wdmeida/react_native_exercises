75b5b8eaaa97adc64e4e92b2e84601ceee66a647
Wagner Almeida <wdmeida@gmail.com>
2017-04-22 00:36:49 -0300

    Refatoração dos dois exemplos para corrigir erros de build.

--- app2/index.android.js
+++ app2/index.android.js
@@ -1,20 +1,18 @@
 //Import
 import React from 'react';
-import {Text, AppRegistry} from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
 //Formatações
 const Estilos = {
   estiloTexto: {
-    fontSize: 50
+    fontSize: 50 //Unidade padrão dp.
   }
 };
 
 //Cria o componente
-const App = () => {
-  return (
-    <Text style={ Estilos.estiloTexto }>Frases do dia</Text>
+const App = () => (
+    <Text style={Estilos.estiloTexto}>Frases do dia</Text>
   );
-};
 
 //Renderiza para o dispositivo.
 AppRegistry.registerComponent('app2', () => App);
