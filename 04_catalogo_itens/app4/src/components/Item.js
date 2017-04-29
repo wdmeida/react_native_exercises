7f28b1c2b2e734be4c062c34339f19e4b0f06291
Wagner Almeida <wdmeida@gmail.com>
2017-04-29 18:40:17 -0300

    Implementação da requisição e listagem dos produtos do app4.

--- 04_catalogo_itens/app4/src/components/Item.js
+++ 04_catalogo_itens/app4/src/components/Item.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default class Item extends Component {
     render() {
         return (
-            <Text>Eu sou um item</Text>
+            <View style={styles.containerItem}>
+                
+                <View style={styles.containerImg}>
+                    <Image style={styles.img} source={{ uri: this.props.item.foto }} />
+                </View>
+
+                <View style={styles.detailsItems}>
+                    <Text style={styles.title}>{ this.props.item.titulo }</Text>
+                    <Text style={styles.value}>R$ { this.props.item.valor }</Text>
+                    <Text style={styles.text}>Local: { this.props.item.local_anuncio }</Text>
+                    <Text style={styles.text}>Data publicação:{ this.props.item.data_publicacao }</Text>                
+                </View>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    containerItem: {
+        backgroundColor: '#FFF',
+        borderWidth: 0.5,
+        borderColor: '#999',
+        margin: 10,
+        padding: 10,
+        flexDirection: 'row'
+    },
+    containerImg: {
+        height: 102,
+        width: 102
+    },
+    img: {
+        height: 100,
+        width: 100
+    },
+    detailsItems: {
+        marginLeft: 20,
+        flex: 1
+    },
+    title: {
+        fontSize: 18,
+        color: 'blue',
+        marginBottom: 5
+    },
+    value: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    text: {
+        fontSize: 15
+    }
+});
--- 04_catalogo_itens/app4/src/components/ListaItens.js
+++ 04_catalogo_itens/app4/src/components/ListaItens.js
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
+import Axios from 'axios';
 
 import Item from './Item';
 
+const urlRequest = 'http://faus.com.br/recursos/c/dmairr/api/itens.html';
+
 export default class ListaItens extends Component {
-    
+
+  constructor(props) {
+    super(props);
+
+    this.state = { listaItens: [] };
+  }
+
+  componentWillMount() {
+    //request
+    Axios.get(urlRequest)
+      .then(response => { this.setState({ listaItens: response.data }); })
+      .catch(() => console.log('Erro ao recuperar os dados'));
+  }
+
   render() {
     return (
-      <View>
-        <Item />
-        <Item />
-        <Item />
-      </View>
+      <ScrollView style={{ backgroundColor: '#DDD' }}>
+        { this.state.listaItens.map(item => (<Item key={item.titulo} item={item} />))}
+      </ScrollView>
     );
   }
 }
